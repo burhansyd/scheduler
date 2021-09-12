4b9a422dede9b81b50456ca800811a4a78122d59
Burhan <burhan.syed@outlook.com>
2021-09-12 18:57:42 -0400

    implement deleting and editing appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -38,9 +38,20 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     };
-    setState({...state, appointments});
-    axios.put(`/api/appointments/${id}`, {interview}).then(response => console.log(response));
+    axios.put(`/api/appointments/${id}`, {interview}).then(setState({...state, appointments}));
   };
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    axios.delete(`/api/appointments/${id}`).then(setState({...state, appointments}));
+  }
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
@@ -56,6 +67,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={interviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,12 +6,16 @@ import Empty from "./Empty";
 import Status from "./Status";
 import { useVisualMode } from "hooks/useVisualMode";
 import Form from "./Form";
+import Confirm from "./Confirm";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   )
@@ -24,6 +28,20 @@ export default function Appointment(props) {
     props.bookInterview(props.id, interview);
     transition(SHOW);
   };
+  function confirmer() {
+    transition(CONFIRM);
+  };
+  function canceler() {
+    transition(SHOW);
+  };
+  function deleter() {
+    transition(DELETING);
+    props.cancelInterview(props.id);
+    transition(EMPTY);
+  };
+  function editer() {
+    transition(EDIT);
+  }
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -32,10 +50,15 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={confirmer}
+          onEdit={editer}
         />
       )}
       {mode === CREATE && <Form interviewers={props.interviewers} onCancel={() => back(EMPTY)} onSave={save}/>}
       {mode === SAVING && <Status message="Saving" />}
+      {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && <Confirm message="Are you sure you would like to delete?" onCancel={canceler} onConfirm={deleter} />}
+      {mode === EDIT && <Form interviewers={props.interviewers} interviewer={props.interview.interviewer.id} name={props.interview.student} onCancel={() => back(SHOW)} onSave={save} />}
     </article>
   )
 }
